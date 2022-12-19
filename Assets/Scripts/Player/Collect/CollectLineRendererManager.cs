9e7c0c80cb7c227c4d92a2311a8720979d60192f
Beytullah KALAY <beytullah.kalay@outlook.com>
2022-12-19 14:33:05 +0300

    add: object pooling system to line renderer feature

--- Assets/Scripts/Player/Collect/CollectLineRendererManager.cs
+++ Assets/Scripts/Player/Collect/CollectLineRendererManager.cs
@@ -1,23 +1,45 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class CollectLineRendererManager : MonoBehaviour
 {
     [SerializeField] private GameObject collectLineRenderer;
-    
-    private Dictionary<Orb, GameObject> _lineRenderers = new Dictionary<Orb, GameObject>();
 
-    public void CreateConnectedLineRenderer(Transform playerTransform, Transform orbTransform,Orb orb)
+    private readonly Dictionary<Orb, GameObject> _lineRenderers = new Dictionary<Orb, GameObject>();
+
+    private ObjectPool<GameObject> _pool;
+
+    private void Start()
     {
-        // create line renderer game object
-        var rendererGameObject = Instantiate(collectLineRenderer);
+        _pool = new ObjectPool<GameObject>(() =>
+            {
+                return Instantiate(collectLineRenderer);
+            }, lineRenderer =>
+            {
+                lineRenderer.SetActive(true);
+            }, lineRenderer =>
+            {
+                lineRenderer.SetActive(false);
+                lineRenderer.GetComponent<LineRenderer>().SetPosition(0,Vector3.zero);
+                lineRenderer.GetComponent<LineRenderer>().SetPosition(1,Vector3.zero);
+            }, lineRenderer =>
+            {
+                Destroy(lineRenderer);
+            }, true, 10, 20);
+    }
+
+    public void CreateConnectedLineRenderer(Transform playerTransform, Transform orbTransform, Orb orb)
+    {
+        // get line renderer game object
+        var rendererGameObject = _pool.Get();
 
         // TODO: get rid of GetComponent
         // get collect line renderer
         var lineRenderer = rendererGameObject.GetComponent<CollectLineRenderer>();
-        
+
         // add line renderer object to list
-        _lineRenderers.Add(orb,rendererGameObject);
+        _lineRenderers.Add(orb, rendererGameObject);
 
         // assign line renderer point transforms
         AssignLineRendererPointTransforms(playerTransform, orbTransform, lineRenderer);
@@ -32,9 +54,7 @@ public class CollectLineRendererManager : MonoBehaviour
 
     public void RemoveLineRenderer(Orb orb)
     {
-        var lineRendererObj = _lineRenderers[orb];
+        _pool.Release(_lineRenderers[orb]);
         _lineRenderers.Remove(orb);
-        Destroy(lineRendererObj);
     }
-    
 }
